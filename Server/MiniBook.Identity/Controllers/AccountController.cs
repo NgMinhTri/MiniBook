a7344b025de2793728635ef39265e43ad615fab1
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2020-12-01 23:42:49 +0700

    Tạo API đanngư kí tài khoản

---
+++ Server/MiniBook.Identity/Controllers/AccountController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using MiniBook.Identity.Models;
+using MiniBook.Identity.ViewModels;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace MiniBook.Identity.Controllers
+{
+    [Route("api/[controller]")]
+    public class AccountController : Controller
+    {
+        UserManager<User> UserManager { get; }
+        public AccountController(UserManager<User> userManager)
+        {
+            UserManager = userManager;
+        }
+
+       
+
+
+        [HttpPost]
+       public async Task<IActionResult> Register([FromBody]RegisterViewModel model)
+        {
+            var user = new User()
+            {
+                Email = model.Email,
+                UserName = model.Email
+            };
+            var result = await UserManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+                return Ok();
+            else
+                return BadRequest();
+        }
+    }
+}
