61fc48304a0157f69ef091e8041eab1b2ccd77d4
NgMinhTri <38482726+NgMinhTri@users.noreply.github.com>
2020-12-03 14:22:56 +0700

    API Response Format

---
+++ Server/MiniBook.Server.Shared/ApiResultExtensions.cs
@@ -0,0 +1,43 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Net;
+using System.Text;
+
+namespace MiniBook.Server.Shared
+{
+    public static class ApiResultExtensions
+    {
+        public static IActionResult ErrorResult(this Controller controller, int errorCode, string errorMessage)
+        {
+            return JsonResult(new ApiResponse<object>(errorCode, errorMessage), HttpStatusCode.BadRequest);
+        }
+
+        public static IActionResult ErrorResult(this Controller controller, int errorCode, string errorMessage, HttpStatusCode statusCode)
+        {
+            return JsonResult(new ApiResponse<object>(errorCode, errorMessage), statusCode);
+        }
+
+        public static IActionResult OkResult<T>(this Controller controller, T result)
+        {
+            return JsonResult(new ApiResponse<T>(result));          
+        }
+
+        public static IActionResult OkResult(this Controller controller)
+        {
+            return JsonResult(new ApiResponse<object>(true));
+        }
+
+        public static IActionResult OkResult(this Controller controller, object result)
+        {
+            return JsonResult(new ApiResponse<object>(result));
+        }
+
+        private static IActionResult JsonResult(object result, HttpStatusCode statusCode = HttpStatusCode.OK)
+        {
+            return new ApiJsonResult(result, statusCode);
+            
+            
+        }
+    }
+}
